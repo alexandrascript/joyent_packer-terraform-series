5707774c6b98046359ce84e6c57e16d5dad79a37
Alexandra White <alexandra.n.white@gmail.com>
2018-01-30 15:13:12 -0500

    format 3

--- 3-blue-green-deploys/happy-randomizer/main.tf
+++ 3-blue-green-deploys/happy-randomizer/main.tf
@@ -3,10 +3,7 @@ terraform {
 }
 
 provider "triton" {
-    # Commenting these out because they will take the SDC_URL, SDC_ACCOUNT, and SDC_KEY_ID env vars as defaults
-    # url = "https://${var.dc_name}.api.joyent.com"
-    # account = "${var.triton_account}"
-    # key_id = "${var.triton_key_id}"
+  # Commenting these out because they will take the SDC_URL, SDC_ACCOUNT, and SDC_KEY_ID env vars as defaults  # url = "https://${var.dc_name}.api.joyent.com"  # account = "${var.triton_account}"  # key_id = "${var.triton_key_id}"
 }
 
 #
@@ -14,47 +11,49 @@ provider "triton" {
 #
 data "triton_network" "service_networks" {
   count = "${length(var.service_networks)}"
-  name = "${element(var.service_networks, count.index)}"
+  name  = "${element(var.service_networks, count.index)}"
 }
 
 #
 # Details about the "blue" deployment
 #
 data "triton_image" "blue_image" {
-    name = "${var.blue_image_name}"
-    version = "${var.blue_image_version}"
-    type = "lx-dataset"
-    most_recent = true
+  name        = "${var.blue_image_name}"
+  version     = "${var.blue_image_version}"
+  type        = "lx-dataset"
+  most_recent = true
 }
 
 resource "triton_machine" "blue_machine" {
-    count = "${var.blue_count}"
-    name = "blue_happy_${count.index + 1}"
-    package = "${var.blue_package_name}"
-    image = "${data.triton_image.blue_image.id}"
-    networks = ["${data.triton_network.service_networks.*.id}"]
-    cns {
-        services = ["${var.service_production == "blue" ? var.service_name : "staging-${var.service_name}" }", "blue-${var.service_name}"]
-    }
+  count    = "${var.blue_count}"
+  name     = "blue_happy_${count.index + 1}"
+  package  = "${var.blue_package_name}"
+  image    = "${data.triton_image.blue_image.id}"
+  networks = ["${data.triton_network.service_networks.*.id}"]
+
+  cns {
+    services = ["${var.service_production == "blue" ? var.service_name : "staging-${var.service_name}" }", "blue-${var.service_name}"]
+  }
 }
 
 #
 # Details about the "green" deployment
 #
 data "triton_image" "green_image" {
-    name = "${var.green_image_name}"
-    version = "${var.green_image_version}"
-    type = "lx-dataset"
-    most_recent = true
+  name        = "${var.green_image_name}"
+  version     = "${var.green_image_version}"
+  type        = "lx-dataset"
+  most_recent = true
 }
 
 resource "triton_machine" "green_machine" {
-    count = "${var.green_count}"
-    name = "green_happy_${count.index + 1}"
-    package = "${var.green_package_name}"
-    image = "${data.triton_image.green_image.id}"
-    networks = ["${data.triton_network.service_networks.*.id}"]
-    cns {
-        services = ["${var.service_production == "green" ? var.service_name : "staging-${var.service_name}" }", "green-${var.service_name}"]
-    }
+  count    = "${var.green_count}"
+  name     = "green_happy_${count.index + 1}"
+  package  = "${var.green_package_name}"
+  image    = "${data.triton_image.green_image.id}"
+  networks = ["${data.triton_network.service_networks.*.id}"]
+
+  cns {
+    services = ["${var.service_production == "green" ? var.service_name : "staging-${var.service_name}" }", "green-${var.service_name}"]
+  }
 }
